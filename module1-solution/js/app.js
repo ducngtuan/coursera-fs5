3e556e631f0d2ee5fe787cced78492845766bfba
Duc Nguyen <duc.ngtuan@gmail.com>
2016-09-18 03:58:32 +0200

    do not count empty items when checking

--- module1-solution/js/app.js
+++ module1-solution/js/app.js
@@ -7,19 +7,25 @@
   LunchCheckController.$inject = ['$scope'];
   function LunchCheckController($scope) {
     $scope.check = function() {
-      if ($scope.lunchMenu === "" || angular.isUndefined($scope.lunchMenu)) {
+      if (isEmpty($scope.lunchMenu)) {
         $scope.message = "Please enter data first";
         $scope.isValid = false;
         return;
       }
 
       var items = $scope.lunchMenu.split(",");
-      if (items.length <= 3) {
+      var validItems = items.filter(function(item) { return !isEmpty(item); });
+      if (validItems.length <= 3) {
         $scope.message = "Enjoy!";
       } else {
         $scope.message = "Too much!";
       }
       $scope.isValid = true;
     };
+  }
+
+  // check if a value is an empty string
+  function isEmpty(value) {
+    return angular.isUndefined(value) || (angular.isString(value) && value.trim() === "");
   }
 })();
