9ef876c541378eabd58b4b06b6a69b258e8f05e8
Duc Nguyen <duc.ngtuan@gmail.com>
2016-09-18 03:13:42 +0200

    implement basic solution

--- module1-solution/js/app.js
+++ module1-solution/js/app.js
@@ -2,10 +2,22 @@
   'use strict';
 
   angular.module('LunchCheck', [])
-  .controller('LunchCheckController', [LunchCheckController]);
+  .controller('LunchCheckController', LunchCheckController);
 
   LunchCheckController.$inject = ['$scope'];
   function LunchCheckController($scope) {
+    $scope.check = function() {
+      if ($scope.lunchMenu === "" || angular.isUndefined($scope.lunchMenu)) {
+        $scope.message = "Please enter data first";
+        return;
+      }
 
-  };
+      var items = $scope.lunchMenu.split(",");
+      if (items.length <= 3) {
+        $scope.message = "Enjoy!";
+      } else {
+        $scope.message = "Too much!";
+      }
+    };
+  }
 })();
